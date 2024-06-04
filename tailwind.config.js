5b9ebb033ecd2391aee8bbbeaec6f59d6f67ae07
tasneem said <69201333+tasneemsaeed23@users.noreply.github.com>
2024-06-04 10:01:02 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#867070",
+        secondary: "#D5B4B4",
+        accent: "#E4D0D0",
+        background: "#F5EBEB",
+      },
+    },
+  },
+  plugins: [],
+};
